06fc7af38b8979695a26d6e1d05da8f5b0f3d14d
Yusuf Eken <168830445+Yusuf-Eken@users.noreply.github.com>
2024-06-13 11:42:44 +0300

    app.js

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ import dotenv from "dotenv"
 import conn from "./db.js";
 import pageRoute from "./routes/pageRoute.js"
 import photoRoute from "./routes/photoRoute.js"
+import userRoute from "./routes/userRoute.js"
+
 dotenv.config();
 
 
@@ -18,10 +20,14 @@ app.set('view engine', 'ejs')
 // Static files middleware statik dosyalarımızın public dosyasında olduğunu göstermemize yarar.
 app.use(express.static('public'))
 app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
 //Çalışmalarımız boyunca yapıcağımız her şeyin temeli burada
 //Bir node js web uygulaması request ve respons dan ibarettir
 app.use("/", pageRoute)
 app.use("/photos", photoRoute)
+app.use("/users", userRoute)
+
 
 app.listen(port, () => {
     console.log(`Uygulamanın Çalıştığı Port: ${port}`);
