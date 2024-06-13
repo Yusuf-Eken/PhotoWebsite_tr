7c009b0a9223179e819dbf9f644ba9c5fa0eedf8
Yusuf Eken <168830445+Yusuf-Eken@users.noreply.github.com>
2024-06-13 11:35:24 +0300

    Add files via upload

---
+++ routes/userRoute.js
@@ -0,0 +1,10 @@
+import express from "express"
+import * as userController from "../controllers/userController.js"
+
+const router = express.Router()
+
+router.route("/register").post(userController.createUser);
+router.route("/login").post(userController.loginUser);
+
+
+export default router
